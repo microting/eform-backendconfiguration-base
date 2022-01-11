2b69542438d524f29c711cec236d3ace4f895d7c
René Schultz Madsen <rm@microting.com>
2022-01-11 09:54:17 +0100

    Adding migration to fix the AreaRulesPlanningVersions table.

---
+++ Microting.EformBackendConfigurationBase/Migrations/20220111084957_RenamingAreaRulesIdToAreaRuleId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microting.EformBackendConfigurationBase.Migrations
+{
+    public partial class RenamingAreaRulesIdToAreaRuleId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AreaRulesId",
+                table: "AreaRulesPlanningVersions",
+                "AreaRuleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AreaRuleId",
+                table: "AreaRulesPlanningVersions",
+                "AreaRulesId");
+        }
+    }
+}
