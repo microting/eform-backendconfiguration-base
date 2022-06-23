b50b144915b653ba1c447e681a81235bb12d16e0
René Schultz Madsen <rm@microting.com>
2022-06-23 20:50:03 +0200

    Allowing the values to be null on PoolHourResults.

--- Microting.EformBackendConfigurationBase/Infrastructure/Data/Entities/PoolHourResult.cs
+++ Microting.EformBackendConfigurationBase/Infrastructure/Data/Entities/PoolHourResult.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using JetBrains.Annotations;
 
 namespace Microting.EformBackendConfigurationBase.Infrastructure.Data.Entities;
 
@@ -12,21 +13,21 @@ public class PoolHourResult : PnBase
     public int PlanningId { get; set; }
     [ForeignKey("AreaRule")]
     public int AreaRuleId { get; set; }
-    public double PulseRateAtOpening { get; set; }
-    public double ReadPhValue { get; set; }
-    public double ReadFreeChlorine { get; set; }
-    public double ReadTemperature { get; set; }
-    public double NumberOfGuestsAtClosing { get; set; }
-    public string Clarity { get; set; }
-    public double MeasuredFreeChlorine { get; set; }
-    public double MeasuredTotalChlorine { get; set; }
-    public double MeasuredBoundChlorine { get; set; }
-    public double MeasuredPh { get; set; }
-    public string AcknowledgmentOfPulseRateAtOpening { get; set; }
-    public double MeasuredTempDuringTheDay { get; set; }
-    public string Comment { get; set; }
-    public int DoneByUserId { get; set; }
+    public double? PulseRateAtOpening { get; set; }
+    public double? ReadPhValue { get; set; }
+    public double? ReadFreeChlorine { get; set; }
+    public double? ReadTemperature { get; set; }
+    public double? NumberOfGuestsAtClosing { get; set; }
+    [CanBeNull] public string Clarity { get; set; }
+    public double? MeasuredFreeChlorine { get; set; }
+    public double? MeasuredTotalChlorine { get; set; }
+    public double? MeasuredBoundChlorine { get; set; }
+    public double? MeasuredPh { get; set; }
+    [CanBeNull] public string AcknowledgmentOfPulseRateAtOpening { get; set; }
+    public double? MeasuredTempDuringTheDay { get; set; }
+    [CanBeNull] public string Comment { get; set; }
+    public int? DoneByUserId { get; set; }
     public int FolderId { get; set; }
-    public string DoneByUserName { get; set; }
+    [CanBeNull] public string DoneByUserName { get; set; }
 
 }
--- Microting.EformBackendConfigurationBase/Infrastructure/Data/Entities/PoolHourResultVersion.cs
+++ Microting.EformBackendConfigurationBase/Infrastructure/Data/Entities/PoolHourResultVersion.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using JetBrains.Annotations;
 using Microting.eFormApi.BasePn.Infrastructure.Database.Base;
 
 namespace Microting.EformBackendConfigurationBase.Infrastructure.Data.Entities;
@@ -13,20 +14,20 @@ public class PoolHourResultVersion : BaseEntity
     public int SdkCaseId { get; set; }
     public int PlanningId { get; set; }
     public int AreaRuleId { get; set; }
-    public double PulseRateAtOpening { get; set; }
-    public double ReadPhValue { get; set; }
-    public double ReadFreeChlorine { get; set; }
-    public double ReadTemperature { get; set; }
-    public double NumberOfGuestsAtClosing { get; set; }
-    public string Clarity { get; set; }
-    public double MeasuredFreeChlorine { get; set; }
-    public double MeasuredTotalChlorine { get; set; }
-    public double MeasuredBoundChlorine { get; set; }
-    public double MeasuredPh { get; set; }
-    public string AcknowledgmentOfPulseRateAtOpening { get; set; }
-    public double MeasuredTempDuringTheDay { get; set; }
-    public string Comment { get; set; }
+    public double? PulseRateAtOpening { get; set; }
+    public double? ReadPhValue { get; set; }
+    public double? ReadFreeChlorine { get; set; }
+    public double? ReadTemperature { get; set; }
+    public double? NumberOfGuestsAtClosing { get; set; }
+    [CanBeNull] public string Clarity { get; set; }
+    public double? MeasuredFreeChlorine { get; set; }
+    public double? MeasuredTotalChlorine { get; set; }
+    public double? MeasuredBoundChlorine { get; set; }
+    public double? MeasuredPh { get; set; }
+    [CanBeNull] public string AcknowledgmentOfPulseRateAtOpening { get; set; }
+    public double? MeasuredTempDuringTheDay { get; set; }
+    [CanBeNull] public string Comment { get; set; }
     public int DoneByUserId { get; set; }
     public int FolderId { get; set; }
-    public string DoneByUserName { get; set; }
+    [CanBeNull] public string DoneByUserName { get; set; }
 }
