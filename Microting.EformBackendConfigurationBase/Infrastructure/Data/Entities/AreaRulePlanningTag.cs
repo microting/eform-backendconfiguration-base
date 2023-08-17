e72b758c3ba161e8b849aa057705e81b07bbbb26
Alex Horban <sahok6264@gmail.com>
2023-08-17 20:51:41 +0300

    Add link between ItemPlanningTags and AreaRulePlanning
    - Added new entities: AreaRulePlanningTag and AreaRulePlanningTagVersion
    - Created a link between AreaRulePlanning and AreaRulePlanningTag
    - Added navigation properties to the entities

---
+++ Microting.EformBackendConfigurationBase/Infrastructure/Data/Entities/AreaRulePlanningTag.cs
@@ -0,0 +1,10 @@
+﻿namespace Microting.EformBackendConfigurationBase.Infrastructure.Data.Entities;
+
+public class AreaRulePlanningTag : PnBase
+{
+    public int AreaRulePlanningId { get; set; }
+
+    public virtual AreaRulePlanning AreaRulePlanning { get; set; }
+
+    public int ItemPlanningTagId { get; set; }
+}
---
+++ Microting.EformBackendConfigurationBase/Migrations/20230817153730_AddLinkWithItemPlanningTagsAndAreaRulePlanning.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microting.EformBackendConfigurationBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinkWithItemPlanningTagsAndAreaRulePlanning : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AreaRulePlanningTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AreaRulePlanningId = table.Column<int>(type: "int", nullable: false),
+                    ItemPlanningTagId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    WorkflowState = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedByUserId = table.Column<int>(type: "int", nullable: false),
+                    UpdatedByUserId = table.Column<int>(type: "int", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AreaRulePlanningTags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AreaRulePlanningTags_AreaRulePlannings_AreaRulePlanningId",
+                        column: x => x.AreaRulePlanningId,
+                        principalTable: "AreaRulePlannings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AreaRulePlanningTagVersion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AreaRulePlanningTagId = table.Column<int>(type: "int", nullable: false),
+                    AreaRulePlanningId = table.Column<int>(type: "int", nullable: false),
+                    ItemPlanningTagId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    WorkflowState = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedByUserId = table.Column<int>(type: "int", nullable: false),
+                    UpdatedByUserId = table.Column<int>(type: "int", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AreaRulePlanningTagVersion", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AreaRulePlanningTags_AreaRulePlanningId",
+                table: "AreaRulePlanningTags",
+                column: "AreaRulePlanningId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AreaRulePlanningTags");
+
+            migrationBuilder.DropTable(
+                name: "AreaRulePlanningTagVersion");
+        }
+    }
+}
